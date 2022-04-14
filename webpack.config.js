dd95831803fe94982d641644ecc0d5c5ea3dbed2
chriskorsak <chris@chriskorsak.com>
2022-04-14 15:44:17 -0700

    add htmlwebpack plugin, content hash for main.js file name caching, source-map, webpack dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { SourceMap } = require('module');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    // hash will change on file change, good for browser caching
+    filename: '[name][contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+    // auto open browser after running dev server
+    open: true,
+    // auto refresh browser on file changes
+    hot: true,
   },
   module: {
     rules: [
@@ -22,4 +38,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Webpack App',
+    filename: 'index.html',
+    template: 'src/templates/template.html'
+  }), 
+  ],
 };
